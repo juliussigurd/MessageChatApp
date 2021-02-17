ab9a4c4170219e0b0b30c8612a094e190db36c22
julius heilmann <juliussigurd@live.dk>
2021-02-17 14:21:59 +0100

    add users resource

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Julius", password: "password")
+User.create(username: "Rasmus", password: "password")
+User.create(username: "Andreas", password: "password")
+User.create(username: "Nicolai", password: "password")
+User.create(username: "Jesper", password: "password")
